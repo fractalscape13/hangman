57371a82a2f64b0ef8a350232e40b2233a30f73d
Drake Wilcox <drakewilcox@gmail.com>
2020-04-29 14:16:29 -0700

    add styles components to ul.
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const Word = props => {
+  const ulStyled = styled.ul`
+    display: inline-block;
+    li {
+      display: inline-block;
+      border-bottom: 1px solid black;
+    }
+    span {
+      visibility: hidden;
+    }
+    span.visible {
+      visibility: visible;
+    }
+`;
   const secretArr = props.secret.split('');
   return (
-    <ul>
+    <ulStyled>
       {secretArr.map((char, i) => 
         <li key={i}>
           <span className={props.guesses.includes(char)?'visible':''}>{char}</span>
         </li>
       )}
-    </ul>
+    </ulStyled>
   )
 };
 
