026a76e6958d7ac99edfc72ff9fb8165501c4a17
Drake Wilcox <drakewilcox@gmail.com>
2020-04-29 11:13:37 -0700

    create rest of initial component files.

---
+++ src/components/GameControl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Letters from './Letters';
+import Button from './Button';
+
+
+class GameControl extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    
+  }
+};
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <h1>H A N G M A N</h1>
+  );
+}
+
+export default Header;
