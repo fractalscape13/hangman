ca5cc07c2504098b0e585701f6ac4e44be841bf2
Drake Wilcox <drakewilcox@gmail.com>
2020-04-30 14:20:03 -0700

    add action types and action creators.
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -4,3 +4,4 @@ export const GUESS = 'GUESS';
 export const NEW_GAME = 'NEW_GAME';
 export const WIN_GAME = 'WIN_GAME';
 export const LOSE_GAME = 'LOSE_GAME';
+export const SET_WORD = 'SET_WORD';
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
+import * as c from './ActionTypes';
+
 export const correctGuess = (guess) => {
   const { secretWord, correctLetters, missedLetters, currentDiagram, char } = guess;
   return {
-    type: 'GUESS',
+    type: c.GUESS,
     secretWord: secretWord.filter(x => x !== char),
     correctLetters: correctLetters.push(char),
     missedLetters: missedLetters,
@@ -12,10 +14,44 @@ export const correctGuess = (guess) => {
 export const wrongGuess = (guess) => {
   const { secretWord, correctLetters, missedLetters, currentDiagram, char } = guess;
   return {
-    type: 'GUESS',
+    type: c.GUESS,
     secretWord: secretWord,
     correctLetters: correctLetters,
     missedLetters: missedLetters.push(char),
     currentDiagram: currentDiagram + 1
+  }
+}
+
+export const newGame = () => {
+  // const { displayWord, secretWord, secretWord, win, lose, correctLetters, missedLetters, currentDiagram } = game;
+  return {
+    type: c.NEW_GAME,
+    // displayWord: displayWord,
+    // secretWord: secretWord,
+    // win: win,
+    // lose: lose,
+    // correctLetters: correctLetters,
+    // missedLetters: missedLetters,
+    // currentDiagram: currentDiagram
+  }
+}
+
+export const setWord = () => {
+  return {
+    type: c.SET_WORD
+  }
+}
+
+export const winGame = () => {
+  return {
+    type: c.WIN_GAME,
+    win : true
+  }
+}
+
+export const loseGame = () => {
+  return {
+    type: c.LOSE_GAME,
+    lose : true
   }
 }
