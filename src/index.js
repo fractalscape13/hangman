878171f0b73d62b94f7d702e8ca4e283447c45ee
Drake Wilcox <drakewilcox@gmail.com>
2020-04-30 14:22:18 -0700

    add root reducer.
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import secretWordList from './initialState.js'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers/index';
 
 const store = createStore(
-  rootReducer,
-  secretWordList
+  rootReducer
   );
 
 ReactDOM.render(
