3b01bd4d0d4023a9920a4b6fe23d79abe936d0c9
Drake Wilcox <drakewilcox@gmail.com>
2020-04-30 10:04:14 -0700

    combine reducers.
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import gameControlReducer from './game-control-reducer';
+import winStateReducer from './win-state-reducer';
 
 const rootReducer = combineReducers({
-
+  winState : winStateReducer,
+  gameState: gameControlReducer
 });
 
 export default rootReducer;
